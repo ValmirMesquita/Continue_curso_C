dbe0f120271c58f60b645c84b49d98921a1444d7
Valmir L Sousa Mesquita <valmirsmesquita@gmail.com>
2025-04-15 16:06:45 -0300

    ponteiro

---
+++ avancadoParte1/ponteiros.c
@@ -0,0 +1,30 @@
+/*
+  ========================================
+  Projeto: ponteiros.c
+  Auto: Valmir S Mesquita
+  Data:07/04/2025
+  ========================================
+*/
+
+/*Declaraçao de Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h> 
+
+/************* Prototipos das funcões **********/
+
+
+
+/************** Função principal *************/
+int main() {
+    
+
+    
+    
+    system ("PAUSE");
+    return 0;
+    
+}/****************** End main **********************/
+
+/*********** Desenvolvimentos das Funcões ***********/ 
