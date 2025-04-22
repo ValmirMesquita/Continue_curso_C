8c440a704fd3f8f5f602d5491b74afce705b26c7
Valmir L Sousa Mesquita <valmirsmesquita@gmail.com>
2025-04-22 09:09:24 -0300

    ponteiros_vetores

--- avancadoParte1/ponteiros_vetores.c
+++ avancadoParte1/ponteiros_vetores.c
@@ -20,30 +20,20 @@
 /************** Função principal *************/
 int main() {
   int count;
-  int segun;
   
   printf("Digite um numero: ");
   scanf("%d", &count);
 
   for (int i = 0; i < count; i++)
   {
-    printf("%d : %d", i, count);
+    for (int j = 0; j < count; j++){
+    printf("%d : %d", i, j);
     sleep(1);
-    system("cls");  
+    system("cls");
+    }
   }
 
-  if (count == 60)
-  {
-    for (int i = 0; i < segun; i++)
-      {
-        printf("%d : %d", i, i);
-        sleep(1);
-        system("cls");  
-      }
-  }
-  printf("----- FIM DA CONTAGEM ----- \n");
-  
-  
+
     system ("PAUSE");
     return 0;
     
