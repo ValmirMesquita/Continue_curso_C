b564a57e4f5080c58a18e7639a0855136b0c9b6b
Valmir L Sousa Mesquita <valmirsmesquita@gmail.com>
2025-04-23 07:32:50 -0300

    avancadoParte1

---
+++ avancadoParte1/JanelaWindows.c
@@ -0,0 +1,32 @@
+/*
+  ========================================
+  Projeto: ponteiros_vetores.c
+  Auto: Valmir S Mesquita
+  Data:07/04/2025
+  ========================================
+*/
+
+/*Declaraçao de Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h> 
+#include <time.h>
+#include <windows.h>
+
+/* Prototipos das funcões */
+
+
+
+/* Função principal */
+int main() {
+  
+    
+
+    
+system ("PAUSE");
+return 0;
+
+}/*End main*/
+
+/* Desenvolvimentos das Funcões */ 
